aa71efcdf128fa1bf112d8b50a5cfe25936fa4aa
Zaki Sheriff <zakisheriff57@gmail.com>
2026-01-02 21:08:50 +0530

    Removed Fireworks from other Pages

--- web/src/components/Fireworks.tsx
+++ web/src/components/Fireworks.tsx
@@ -1,7 +1,16 @@
+"use client";
+
 import React from 'react';
+import { usePathname } from 'next/navigation';
 import './Fireworks.css';
 
 const Fireworks = () => {
+    const pathname = usePathname();
+
+    if (pathname !== '/') {
+        return null;
+    }
+
     return (
         <div className="fireworks-container">
             {/* Apple Blue & Red Fireworks covering more area */}
